695aade0f3b7539dc9286e85962c6ade578469c7
digvijay0899 <78948792+digvijay0899@users.noreply.github.com>
2021-09-18 16:23:36 +0530

    Add files via upload

---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {TodoItem} from "./TodoItem"
+export const Todos = (props) => {
+    let myStyle={
+        minHeight:"60vh",
+        margin:"40px auto"
+    }
+    return (
+
+        <div className="container" style={myStyle}  >
+            <h3 className =" my-3"  >Todos List</h3>
+              {props.todos.length===0? "No Todos to Display":
+              props.todos.map((todo)=>{
+                //  console.log(todo.sno);
+            return (
+            <>
+             <TodoItem todo={todo} 
+             key={todo.sno} 
+             onDelete={props.onDelete}/>
+            <hr/>
+            </>
+              )})}
+        
+        </div>
+    )
+}
+ 
